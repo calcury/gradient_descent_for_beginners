6c62b2a0bbf788ac16649108e70a2a42f42f7e32
calcury <calcury@163.com>
2026-03-13 10:15:49 +0800

    fix: gd visualization

--- src/example.py
+++ src/example.py
@@ -1,7 +1,8 @@
 lr = 0.1
-x = 2
+x = 100
 for _ in range(100):
-    Loss = (x ** 2) / 2 # Loss function
-    Gradient = x # Gradient
-    x = x - lr * Gradient # Update param
-print(f'x:{x:.2f}; Loss:{Loss:.2f}')
+    Loss = (x ** 2) / 2  # Loss function
+    Gradient = x  # Gradient
+    x = x - lr * Gradient  # Update param
+    if _ % 10//9:
+        print(f'Epochs:{_+1}; x:{x:.2f}; Loss:{Loss:.2f}')
